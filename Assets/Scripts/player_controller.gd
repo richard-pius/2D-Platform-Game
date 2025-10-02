332318bd12bb09b4698d588359d1dcfc99f05137
Richard Pius <richardpius2005@gmail.com>
2025-10-02 23:43:24 +0530

    just updating

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -5,21 +5,31 @@ class_name PlayerController
 @export var jump_power = 10.0
 
 var speed_multiplier = 30.0
-var jump_mulitplier = -30.0
+var jump_multiplier = -30.0
 var direction = 0
 
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta: float) -> void:
+func _input(event):
+	# Handle jump.
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+	# Handle jump down
+	if event.is_action_pressed("move_down"):
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
+
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_power * jump_mulitplier
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
